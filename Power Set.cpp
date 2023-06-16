93192c04dd59404e5f83143d82ac20312612098f
021ankit <93592053+021ankit@users.noreply.github.com>
2023-06-16 14:13:13 +0530

    Update Power Set.cpp

--- Power Set.cpp
+++ Power Set.cpp
@@ -1,21 +1,17 @@
 #include <bits/stdc++.h> 
-void solve(int i,int n,vector<int>&v,vector<int>&temp,vector<vector<int>>&res)
-{
-    if(i==n)
-    {
-        res.push_back(temp);
-        return;
-    }
-    solve(i+1,n,v,temp,res);
-    temp.push_back(v[i]);
-    solve(i+1,n,v,temp,res);
-    temp.pop_back();
-}
 vector<vector<int>> pwset(vector<int>v)
 {
-    int n=v.size();
-    vector<int>temp;
     vector<vector<int>>res;
-    solve(0,n,v,temp,res);
+    int n=v.size();
+    for(int i=0; i<(1<<v.size()); i++)
+    {
+        vector<int>temp;
+        for(int j=0; j<n; j++)
+        {
+            if((i&(1<<j))>0)
+             temp.push_back(v[j]);
+        }
+        res.push_back(temp);
+    }
     return res;
 }
